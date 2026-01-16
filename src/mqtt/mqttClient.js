ce61507cbb07059db90989b328f163bb10022ea4
osayl12 <hamedosayl@gmail.com>
2026-01-16 17:18:28 +0200

    FIX MQTT

--- src/mqtt/mqttClient.js
+++ src/mqtt/mqttClient.js
@@ -2,6 +2,16 @@ const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://localhost:1883");
 
 let lastWarning = null;
+let connected = false;
+
+client.on("connect", () => {
+  connected = true;
+  client.subscribe("irrigation/warn");
+});
+
+client.on("close", () => {
+  connected = false;
+});
 
 client.on("message", (topic, payload) => {
   if (topic === "irrigation/warn") {
@@ -13,12 +23,11 @@ client.on("message", (topic, payload) => {
   }
 });
 
-client.on("connect", () => {
-  client.subscribe("irrigation/warn");
-});
-
 module.exports = {
   client,
   getLastWarning: () => lastWarning,
-  clearWarning: () => { lastWarning = null; }
+  clearWarning: () => { lastWarning = null; },
+  getStatus: () => ({
+    mqttConnected: connected
+  })
 };
